8a92ac970549f9f8a2c08a9a76545af192384efc
Jorge Harbes <jharbes@hotmail.com>
2023-06-07 19:44:32 -0300

    Create 03-o_que_eh_o_magic.py

---
+++ 02-streamlite-write-e-magic/03-o_que_eh_o_magic.py
@@ -0,0 +1,25 @@
+# MAGIC é a capacidade de atualizar em tempo real o que for editado no streamlit
+# para isso devemos ligar a opcao 'always rerun' no dashboard
+
+import pandas as pd
+import streamlit as st
+
+df=pd.DataFrame({'col1':[1,2,3]})
+df
+df
+
+
+
+'''
+Com o *Magic* voce pode escrever diretamente no texto
+'''
+
+x='Oi tudo bem?'
+x
+x
+
+z=10
+y=3
+
+z+y
+z*y
