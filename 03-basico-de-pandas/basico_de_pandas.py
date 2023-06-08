1e5cbbe15763e1e8bf9c59247069db46de93657b
Jorge Harbes <jharbes@hotmail.com>
2023-06-08 15:23:55 -0300

    adicionado system extraction

--- 03-basico-de-pandas/basico_de_pandas.py
+++ 03-basico-de-pandas/basico_de_pandas.py
@@ -2,38 +2,38 @@ import pandas as pd
 
 df = pd.read_excel('./system_extraction.xlsx')
 
-df # imprime o dataframe no jupyter ou google colab
+print(df) # imprime o dataframe
 
 
-df.head(5) # imprime apenas as cinco primeiro linhas para dar nocao de como ele é
+print(df.head(5)) # imprime apenas as cinco primeiro linhas para dar nocao de como ele é
 
 
 df = df.drop(columns=['Região', 'Preço']) # deletando as colunas 'Região' e 'Preço'
 
-df
+print(df)
 
 
 
-df.loc[df['Vendedor'] == 'Paulo'] # loc (localize) para filtrar a tabela apenas com o 'Vendedor' que seja 'Paulo"
+print(df.loc[df['Vendedor'] == 'Paulo']) # loc (localize) para filtrar a tabela apenas com o 'Vendedor' que seja 'Paulo"
 
 
-df.loc[df['Vendedor'] == 'Paulo', ['Valor Pedido']] # vai filtar o Vendedor Paulo porém a tabela só retornará a coluna 'Valor Pedido'
-
-
-
-df.groupby(['Produto vendido']).sum() # agrupa pelo produto vendido e soma as colunas
-
-df.groupby(['Produto vendido']).mean() # agrupa pelo produto vendido e mostra a media das colunas
+print(df.loc[df['Vendedor'] == 'Paulo', ['Valor Pedido']]) # vai filtar o Vendedor Paulo porém a tabela só retornará a coluna 'Valor Pedido'
 
 
 
-len(df) # retorna o numero de linhas do dataframe
+print(df.groupby(['Produto vendido']).sum()) # agrupa pelo produto vendido e soma as colunas
+
+print(df.groupby(['Produto vendido']).mean()) # agrupa pelo produto vendido e mostra a media das colunas
+
+
+
+print(len(df)) # retorna o numero de linhas do dataframe
 
 
 
 df['marg_porc'] = 100*df['Margem Lucro'] / df['Valor Pedido'] # criamos a coluna 'marg_porc' que é a margem percentual adicionando ela ao dataframe
 
-df # imprimimos o dataframe
+print(df) # imprimimos o dataframe
 
 
 
