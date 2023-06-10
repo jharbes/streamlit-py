fb5e0b7292cef8e4b6b8436c5a76df92b4373208
Jorge Harbes <jharbes@hotmail.com>
2023-06-10 12:46:37 -0300

    Update 20-como_criar_graficos_de_dispersao.py

--- 04-altair-criacao-de-graficos/20-como_criar_graficos_de_dispersao.py
+++ 04-altair-criacao-de-graficos/20-como_criar_graficos_de_dispersao.py
@@ -2,12 +2,27 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 
-source = pd.read_csv('../Datasets/vega_car.csv')
+df = pd.read_csv('../Datasets/vega_car.csv')
 
-source # imprimindo o dataframe
+df # imprimindo o dataframe
 
 
 # esse dataframe mostra o consumo de gasolina de alguns veículos conforme o numero de cavalos do motor
 # vamos ver a relacao entre o consumo de combustivel e quantos cavalos tem o motor e verificar se conseguimos criar um grafico de dispersao que mostre a variacao dessas duas variaveis.
 
 
+grafico_dispersao=alt.Chart(df).mark_point().encode(
+    
+    # :Q medida quantitativa
+    x='Horsepower:Q',
+
+    y='Miles_per_Gallon',
+
+    # alterando o valor da cor de acordo com o local de origem do carro (europe, usa, japan)
+    color=alt.Color('Origin:N')
+)
+
+
+st.subheader('GRÁFICO DE DISPERSÃO: CONSUMO POR NUMERO DE HORSEPOWER')
+
+st.altair_chart(grafico_dispersao,use_container_width=True)
