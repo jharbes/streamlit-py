47ee0fbd0847eb5361479f0c2fed8852dfe293b6
Jorge Harbes <jharbes@hotmail.com>
2023-06-10 10:54:48 -0300

    Create 19-ok+altair_graf_arc.py

---
+++ 04-altair-criacao-de-graficos/19-ok+altair_graf_arc.py
@@ -0,0 +1,31 @@
+from turtle import color
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+df = pd.read_excel(
+    io = './Datasets/faturamento.xlsx',
+    engine = 'openpyxl',
+    sheet_name='ricos',
+    usecols='A:B',
+    nrows=9
+)
+
+st.subheader('GRÁFICO PIZZA - MAIS RICOS DO MUNDO')
+
+graf_pizza = alt.Chart(df).mark_arc(
+    innerRadius=0,
+    outerRadius=150
+).encode(
+    theta = alt.Theta(field='Fortuna', type='quantitative', stack=True),
+    color=alt.Color(
+        field='Nome', 
+        type='nominal',
+        #legend=None
+    ),
+    tooltip = ['Nome', 'Fortuna']
+).properties(width=700, height=450)
+
+rotuloNome = graf_pizza.mark_text(radius=200, size=14).encode(text='Nome')
+rotuloValor = graf_pizza.mark_text(radius=165, size=14).encode(text='Fortuna')
+st.altair_chart(graf_pizza+rotuloNome+rotuloValor)
