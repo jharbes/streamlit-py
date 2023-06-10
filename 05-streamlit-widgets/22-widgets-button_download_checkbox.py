c2b51ea926b9eef4ad7efe076f57e8c5fb7206c5
Jorge Harbes <jharbes@hotmail.com>
2023-06-10 14:03:08 -0300

    botao e botao de download widgets

--- 05-streamlit-widgets/22-widgets-button_download_checkbox.py
+++ 05-streamlit-widgets/22-widgets-button_download_checkbox.py
@@ -9,3 +9,27 @@ df = pd.read_excel(
     nrows=40,
 )
 
+df # imprime o dataframe
+
+
+st.subheader('Botão')
+
+# podemos adicionar uma açao ao botao por meio de uma condicional
+if st.button('Clique aqui'):
+    st.write('Você me clicou')
+
+
+
+# funcao para converter o dataframe em arquivo csv
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+
+st.subheader('Botão de download')
+# botão de download
+st.download_button(
+    label='Baixar dados em .csv',
+    data=convert_df(df),
+    file_name='df.csv',
+    mime='text/csv'   
+)
