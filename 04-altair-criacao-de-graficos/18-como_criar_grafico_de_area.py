ebb7d349e4a27d3c47e5dd9f53efafd8ec1e5661
Jorge Harbes <jharbes@hotmail.com>
2023-06-10 09:42:48 -0300

    Create 18-como_criar_grafico_de_area.py

---
+++ 04-altair-criacao-de-graficos/18-como_criar_grafico_de_area.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+df = pd.read_excel(
+    io = './Datasets/faturamento.xlsx',
+    engine='openpyxl',
+    sheet_name='flow',
+    usecols='A:B',
+    nrows=15,
+)
