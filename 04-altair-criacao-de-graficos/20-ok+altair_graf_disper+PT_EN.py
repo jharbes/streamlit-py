89ab5d3476ba8ae56da19cf54d480cc726cf1385
Jorge Harbes <jharbes@hotmail.com>
2023-06-10 11:24:41 -0300

    Create 20-ok+altair_graf_disper+PT_EN.py

---
+++ 04-altair-criacao-de-graficos/20-ok+altair_graf_disper+PT_EN.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import altair as alt
+import pandas as pd
+
+source = pd.read_csv('./Datasets/vega_car.csv')
+
+points = alt.Chart(source).mark_point().encode(
+    x='Horsepower:Q',
+    y='Miles_per_Gallon:Q',
+    color=alt.Color('Origin:N')   
+)
+
+st.altair_chart(points, use_container_width=True)
