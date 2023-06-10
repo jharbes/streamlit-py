75fc70c027438aedb6066209e20d267c7bab63c3
Jorge Harbes <jharbes@hotmail.com>
2023-06-10 14:08:08 -0300

    Create 23-widgets-radio-button_select-box_selecao-multipla.py

---
+++ 05-streamlit-widgets/23-widgets-radio-button_select-box_selecao-multipla.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_excel(
+    io = './Datasets/faturamento.xlsx',
+    engine='openpyxl',
+    sheet_name='Interação',
+    usecols='A:C',
+    nrows=40,
+)
+
+
+
+st.subheader('Radio Button')
