38ffedaa9abce88701ec35cb6bf93af83e6c977a
Jorge Harbes <jharbes@hotmail.com>
2023-06-10 09:42:03 -0300

    Create 18-ok+altair_graf_area.py

---
+++ 04-altair-criacao-de-graficos/18-ok+altair_graf_area.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+df = pd.read_excel(
+    io = './Datasets/faturamento.xlsx',
+    engine='openpyxl',
+    sheet_name='flow',
+    usecols='A:B',
+    nrows=15,
+)
+
+graf_area = alt.Chart(df).mark_area(
+    #line={'color':'black'},
+    color='gray'
+).encode(
+    x = 'Year:T',
+    y = 'Value:Q'
+)
+
+rotulo = graf_area.mark_text(
+    align='center',
+    baseline='middle',
+    color='black',    
+    size=14,
+    dy=-18
+).encode(text='Value')
+
+st.subheader('VALORES ANUAIS')
+st.altair_chart(graf_area+rotulo, use_container_width=True)
