a7fce56b5edb3b4764a46b1af5bc296e9342db66
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-16 10:27:31 -0300

    Update 28-como_usar_o_expander.py

--- 06-streamlit-elementos-de-midia-e-temas/28-como_usar_o_expander.py
+++ 06-streamlit-elementos-de-midia-e-temas/28-como_usar_o_expander.py
@@ -1,2 +1,14 @@
 import streamlit as st
 
+st.header('Expander')
+
+st.line_chart({'data':[1,5,2,6,2,1]})
+
+
+# o expander funciona como uma caixa retrátil (pode estar aberta ou fechada) usada em geral para colocar informações acerca de algum assunto, normalmente sobre os graficos ou datasets presentes na pagina
+with st.expander('Ver detalhes'):
+    st.write("""
+    Este é um exemplo de Expander,
+    Você vai ver como usá-lo.
+    """)
+    st.image('../Mídia/dice.jpg')
