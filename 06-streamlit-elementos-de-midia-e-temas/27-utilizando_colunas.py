1c2388c7b45977ea0bd004c8d70f3e7b69d87008
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-16 10:16:32 -0300

    Update 27-utilizando_colunas.py

--- 06-streamlit-elementos-de-midia-e-temas/27-utilizando_colunas.py
+++ 06-streamlit-elementos-de-midia-e-temas/27-utilizando_colunas.py
@@ -1,3 +1,38 @@
 import streamlit as st
 import numpy as np
 
+col1,col2,col3,col4=st.columns(4)
+
+
+with col1:
+    st.subheader('Coluna 1')
+    st.image('../Mídia/cat.jpg')
+
+with col2:
+    st.subheader('Coluna 2')
+    st.image('../Mídia/dog.jpg')
+
+with col3:
+    st.subheader('Coluna 3')
+    st.image('../Mídia/owl.jpg')
+
+with col4:
+    st.subheader('Coluna 4')
+    st.image('../Mídia/dog_2.jpg')
+
+
+# estamos definindo aqui que queremos duas colunas, sendo que a primeira terá o triplo do tamanho da segunda, razao 3:1 entre as colunas
+# gap='large' (small,medium or large) distancia entre as colunas
+
+col_1,col_2=st.columns([3,1],gap='large')
+
+
+# gerando um dataset de numeros aleatorios com 100 numeros em uma coluna
+data=np.random.randn(100,1)
+
+
+# colocaremos o grafico de linha do dataset na primeira coluna
+col_1.line_chart(data)
+
+# na segunda coluna escreveremos os dados referente ao dataset
+col_2.write(data)
