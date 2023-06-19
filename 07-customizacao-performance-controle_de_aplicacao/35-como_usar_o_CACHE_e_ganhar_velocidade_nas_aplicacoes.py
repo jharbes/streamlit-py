ea84ae4c042aed90be5d9edbb0a83f0232b74e9a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-19 10:31:05 -0300

    Update 35-como_usar_o_CACHE_e_ganhar_velocidade_nas_aplicacoes.py

--- 07-customizacao-performance-controle_de_aplicacao/35-como_usar_o_CACHE_e_ganhar_velocidade_nas_aplicacoes.py
+++ 07-customizacao-performance-controle_de_aplicacao/35-como_usar_o_CACHE_e_ganhar_velocidade_nas_aplicacoes.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import streamlit as st
 
-df=pd.read_excel(
-    io='../Datasets/faturamento.xlsx',
-    engine='openpyxl',
-    sheet_name='cache_teste',
-    usecols='A:E',
-    nrows=100000,
-)
+
+# O streamlit vai armazenar no cache da maquina todo o dataset, ou seja, vai carregar todo o dataset na primeira vez e todos os outros filtros serao muito rapidos
+@st.cache_data
+def busca_df():
+    df=pd.read_excel(
+        io='../Datasets/faturamento.xlsx',
+        engine='openpyxl',
+        sheet_name='cache_teste',
+        usecols='A:E',
+        nrows=100000,
+    )
+    return df
+
+
+df=busca_df()
 
 
 # SIDEBAR
@@ -19,3 +27,21 @@ selecao_cliente=st.sidebar.multiselect(
 )
 
 
+# Cria um novo df conforme filtro e cliente
+df_filtro_cliente=df.query(
+    'Cliente == @selecao_cliente'
+)
+
+
+# Adiciona ao side bar a selecao da coluna a exibir
+selecao_colunas=st.sidebar.multiselect(
+    'Selecione a coluna a exibir',
+    options=list(df_filtro_cliente),default=list(df_filtro_cliente)
+)
+
+df_printado=df_filtro_cliente.filter(items=selecao_colunas)
+
+
+
+st.header('Como usar o *CACHE* no **Streamlit** ')
+df_printado
