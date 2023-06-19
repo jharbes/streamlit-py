ecee2fe7e9874a5c6f34c5ab099ecb489bde7414
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-06-19 09:25:52 -0300

    Update 33-use_FORMS_no_streamlit.py

--- 07-customizacao-performance-controle_de_aplicacao/33-use_FORMS_no_streamlit.py
+++ 07-customizacao-performance-controle_de_aplicacao/33-use_FORMS_no_streamlit.py
@@ -2,35 +2,43 @@ import streamlit as st
 import datetime
 
 
-st.subheader('Formulários no Streamlit')
+# Uma opção muito utilizada para formulários é usar um st.form, isso faz com que os dados selecionados só sejam processados após o envio por um botão de submit, evitando alterações em tempo real ao mexer nos formulários, ensejando assim apenas uma atualização única após o envio completo do formulário
+with st.form('Formulário de seleção de parâmetros'):
 
-varClient=st.radio(
-    '1- Selecione o cliente:',
-    ('Space X','Microsoft','Apple','Google','Amazon')
-)
+    st.subheader('Formulários no Streamlit')
 
-varBanco=st.multiselect(
-    '2- Selecione o banco:',
-    ('Bradesco','Itaú','NuBank','Caixa','Banco do Brasil')
-)
+    varClient=st.radio(
+        '1- Selecione o cliente:',
+        ('Space X','Microsoft','Apple','Google','Amazon')
+    )
 
-varParcela=st.slider(
-    '3- Quantas parcelas deseja financiar?',
-    # 0 a 60 será o range total e 30 o valor inicial para o slider
-    0,60,30
-)
+    varBanco=st.multiselect(
+        '2- Selecione o banco:',
+        ('Bradesco','Itaú','NuBank','Caixa','Banco do Brasil')
+    )
 
-varData=st.date_input(
-    '4- Selecione a data de vencimento:',
-    # aqui colocamos a data inicial a aparecer no app, podemos usar datetime.now() para que seja a data atual
-    datetime.date(2023,1,1)
-)
+    varParcela=st.slider(
+        '3- Quantas parcelas deseja financiar?',
+        # 0 a 60 será o range total e 30 o valor inicial para o slider
+        0,60,30
+    )
+
+    varData=st.date_input(
+        '4- Selecione a data de vencimento:',
+        # aqui colocamos a data inicial a aparecer no app, podemos usar datetime.now() para que seja a data atual
+        datetime.date(2023,1,1)
+    )
 
 
-'----------------------------------------------------'
+    '----------------------------------------------------'
 
+    botao_formulario=st.form_submit_button('Filtrar')
+
+st.write('')
+st.write('')
 
 st.write('Cliente: ',varClient)
 st.write('Banco: ',varBanco)
 st.write('Parcelas: ',varParcela)
 st.write('Data de Vencimento: ',varData)
+
